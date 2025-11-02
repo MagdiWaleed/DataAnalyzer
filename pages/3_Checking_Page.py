7b9ea8018baa16e2e2fbd90c4a7cc7c6fa752960
Magdi Waleed <94909647+MagdiWaleed@users.noreply.github.com>
2025-11-02 15:38:33 +0300

    Add Stage Comparison Dashboard page for enhanced data visualization and filtering

---
+++ pages/3_Checking_Page.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from PipeLines import PipeLine
+from measures.Count import CountMeasure
+
+countMeasure = CountMeasure()
+
+
+
+st.set_page_config(
+    page_title="Stage Comparison Dashboard",
+    layout="wide"
+    
+)
+
+st.title("📊 Stage-to-Stage Comparison")
+
+
+if "data" not in st.session_state.keys():
+    pipeLine = PipeLine()
+    data, sheet2 = pipeLine.run(return_sheet2=True)
+    st.session_state['data'] = data
+
+data = st.session_state['data'] 
+
+result,companies = countMeasure.countPerStage(data,unique=True)
+
+with st.expander("Search By Company or Stage"):
+    filter_by = st.text_input("Search About ... ").lower()
+
+    st.divider()
+    for company in companies['result']:
+            if company['name'].lower().__contains__(filter_by) or company['stage'].lower().__contains__(filter_by):
+                st.write(company['name'], " --> ",company['stage'])
+
